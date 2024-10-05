8731c1cbbcaccd199ba76eb672993652da6efa2a
전남주 <79938350+Yirigong@users.noreply.github.com>
2024-10-05 22:34:51 +0900

    Update yazatimes.js

--- yazatimes.js
+++ yazatimes.js
@@ -1,56 +1,52 @@
 function getTodayLabel() {
     var week = new Array('일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일');
     var today = new Date().getDay();
-    var todayLabel = week[today];
-    return todayLabel;
+    return week[today];
 }
 
-today_week = getTodayLabel();
+var today_week = getTodayLabel();
 console.log(today_week);
 
-setTimeout(function(){
+setTimeout(function() {
     location.reload();
-}, 300000);
+}, 300000); // 5분마다 새로고침
 
+// 날짜 포맷을 계산하는 함수
 function getFormattedDate() {
     var now = new Date();
     var d = now.getDate();
-    var m = now.getMonth();
+    var m = now.getMonth() + 1; // 월은 0부터 시작하므로 1 더함
     var y = now.getFullYear();
-    m = m + 1;
+    
     if (m < 10) {
         m = "0" + m;
     }
     if (d < 10) {
         d = "0" + d;
     }
-    return {y: y, m: m, d: d};
+    
+    return { y: y, m: m, d: d };
 }
 
-var date = getFormattedDate();
-var weekdayTime = date.m + "/" + date.d + "/" + date.y + " " + "22:50:00";
-var weekendTime = date.m + "/" + date.d + "/" + date.y + " " + "22:30:00";
-
-switch(today_week){
-    case '토요일':
-    case '일요일':
-        CountDownTimer(weekendTime, 'second_fin');
-        console.log("주말");
-        console.log(weekendTime);
-        break;
-    default:
-        CountDownTimer(weekdayTime, 'second_fin');
-        console.log("평일");
-        console.log(weekdayTime);
-        break;
+function getTargetTime() {
+    var date = getFormattedDate();
+    var weekdayTime = date.m + "/" + date.d + "/" + date.y + " " + "22:50:00";
+    var weekendTime = date.m + "/" + date.d + "/" + date.y + " " + "22:30:00";
+    
+    return (today_week === '토요일' || today_week === '일요일') ? weekendTime : weekdayTime;
 }
+
+var targetTime = getTargetTime();
+console.log("Target Time:", targetTime);
+
+CountDownTimer(targetTime, 'second_fin');
 
 function CountDownTimer(targetTime, id) {
     var end = new Date(targetTime);
+
     var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
-    var _day = _hour * 24;
     var timer;
 
     function showRemaining() {
@@ -65,13 +61,16 @@ function CountDownTimer(targetTime, id) {
             return;
         }
 
-        var hours = Math.floor((distance % _day) / _hour);
+        var hours = Math.floor((distance % (_hour * 24)) / _hour);
         var minutes = Math.floor((distance % _hour) / _minute);
         var seconds = Math.floor((distance % _minute) / _second);
 
         document.getElementById(id).innerHTML = hours + '시간 ' + minutes + '분 ' + seconds + '초';
-        document.getElementById('day-date').innerHTML = date.y + '년 ' + date.m + '월 ' + date.d + '일 ' + today_week + ' 입니다.';
+
+        // 매번 날짜를 최신 값으로 갱신하여 표시
+        var currentDate = getFormattedDate();
+        document.getElementById('day-date').innerHTML = currentDate.y + '년 ' + currentDate.m + '월 ' + currentDate.d + '일 ' + today_week + ' 입니다.';
     }
 
-    timer = setInterval(showRemaining, 1000);
+    timer = setInterval(showRemaining, 1000); // 1초마다 갱신
 }
